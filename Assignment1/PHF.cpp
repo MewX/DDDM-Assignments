b31a773857e49c7e968a50a6664ae57533b8e15a
MewX <imewx@qq.com>
2018-03-24 23:25:32 +1030

    Added input parser logics

--- Assignment1/PHF.cpp
+++ Assignment1/PHF.cpp
@@ -8,8 +8,9 @@
 #include <string>
 #include <map>
 #include <vector>
-#include <set>
 #include <sstream>
+#include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -17,7 +18,6 @@ using namespace std;
 typedef string ColumnName;
 typedef map<ColumnName, string> Record;
 typedef vector<string> ColumnValues;
-typedef map<ColumnName, ColumnValues> Column;
 
 /**
  * Supported operators
@@ -30,6 +30,26 @@ enum Operator
 	LESS_THAN,
 	GREATER_THAN_OR_EQUAL_TO,
 	LESS_THAN_OR_EQUAL_TO,
+};
+
+/**
+ * Global operator mapper
+ */
+const string OP_NOT_EQUAL = "!=";
+const string OP_EQUAL = "="; // after NOT_EQUAL
+const string OP_GREATER_THAN_OR_EQUAL_TO = ">=";
+const string OP_GREATER_THAN = ">"; // after >=
+const string OP_LESS_THAN_OR_EQUAL_TO = "<=";
+const string OP_LESS_THAN = "<"; // after <=
+const vector<pair<string, Operator>> OP_STRING_MAPPER =
+{
+	// the order MATTERS here
+	{OP_NOT_EQUAL, NOT_EQUAL},
+	{OP_GREATER_THAN_OR_EQUAL_TO, GREATER_THAN_OR_EQUAL_TO},
+	{OP_LESS_THAN_OR_EQUAL_TO, LESS_THAN_OR_EQUAL_TO},
+	{OP_EQUAL, EQUAL},
+	{OP_GREATER_THAN, GREATER_THAN},
+	{OP_LESS_THAN, LESS_THAN}
 };
 
 /**
@@ -63,7 +83,7 @@ public:
 	/**
 	* perform the 'not' version of current <predicate>
 	*/
-	void makeAntiVersion()
+	void makeAnti()
 	{
 		op = ANTI_OP_MAP.at(op);
 	}
@@ -72,9 +92,30 @@ public:
 	 * test whether the table record satisfies current predicate.
 	 * @return true if satisfied; otherwise false
 	 */
-	bool satisfy(Record record)
+	bool satisfy(Record record) const
 	{
-		// TODO
+		const string rVal = record.at(key);
+		switch (op)
+		{
+		case EQUAL:
+			return rVal == val;
+
+		case NOT_EQUAL:
+			return rVal != val;
+
+		case GREATER_THAN:
+			return atoi(rVal.c_str()) > atoi(val.c_str());
+
+		case GREATER_THAN_OR_EQUAL_TO:
+			return atoi(rVal.c_str()) >= atoi(val.c_str());
+
+		case LESS_THAN:
+			return atoi(rVal.c_str()) < atoi(val.c_str());
+
+		case LESS_THAN_OR_EQUAL_TO:
+			return atoi(rVal.c_str()) <= atoi(val.c_str());
+		}
+		return false;
 	}
 };
 
@@ -86,39 +127,68 @@ class Table
 {
 private:
 	// The index is currently the "vector<string>"'s index, may add primary key setting
-	vector<Column> db; // db<column names, column values>
+	map<ColumnName, ColumnValues> db; // db<column names, column values>
 
 public:
 	/**
 	 * default constructor
 	 */
-	Table() {}
+	Table() = default;
 
 	/**
 	 * input is the keys' list.
 	 * simply using the read first line, and split with '\t'
 	 */
-	Table(vector<ColumnName> prototypes) : Table()
+	Table(vector<ColumnName> prototype) : Table()
 	{
-		//TODO: to upper case
+		for (auto &str : prototype)
+		{
+			transform(str.begin(), str.end(), str.begin(), ::toupper);
+			db.insert({ str, ColumnValues() });
+		}
+	}
+
+	/**
+	 * get prototypes based on the order it was defined
+	 */
+	vector<ColumnName> getPrototype() const
+	{
+		vector<ColumnName> ret;
+		for (const auto &c : db)
+		{
+			ret.push_back(c.first);
+		}
+		return ret;
 	}
 
 	/**
 	 * get the number of rows of the Table
 	 * @return the size
 	 */
-	int size()
+	int size() const
 	{
-		// TODO:
+		for (const auto &c : db)
+		{
+			return c.second.size();
+		}
+		return 0;
 	}
 
 	/**
 	 * get a record as key-value pair
 	 * @return the <key, value> pair
 	 */
-	Record get(int i)
+	Record get(int i) const
 	{
-		// TODO:
+		// this is an exception!
+		assert(i >= 0 && i < size());
+
+		Record ret;
+		for (const auto &c : db)
+		{
+			ret.insert({ c.first, c.second[i] });
+		}
+		return ret;
 	}
 
 	/**
@@ -127,7 +197,14 @@ public:
 	 */
 	bool add(Record data)
 	{
-		// TODO
+		// validate
+		if (data.size() != db.size()) return false;
+
+		for (const auto &d : data)
+		{
+			db.at(d.first).push_back(d.second);
+		}
+		return true;
 	}
 
 	/**
@@ -136,8 +213,13 @@ public:
 	 */
 	bool del(int i)
 	{
-		// TODO:
+		if (i < 0 || i >= size()) return false;
 
+		for (auto &c : db)
+		{
+			c.second.erase(c.second.begin() + i);
+		}
+		return true;
 	}
 
 };
@@ -145,13 +227,13 @@ public:
 /**
  * a Query is just a set of valid predicates
  */
-typedef set<Predicate> Query;
+typedef vector<Predicate> Query;
 typedef vector<Query> Queries;
 
 /**
  * predicates of the same attribute should be grouped together for its potential helps
  */
-typedef set<Predicate> PredicateGroup; // grouped by attributes
+typedef vector<Predicate> PredicateGroup; // grouped by attributes
 typedef map<string, PredicateGroup> PredicateGroups; // <group attribute, predicates>
 
 /**
@@ -168,7 +250,7 @@ public:
 	/**
 	 * the constructor
 	 */
-	PrimaryHorizentalFragmentation(const Table &db, const Queries queries, const PredicateGroups &predicates)
+	PrimaryHorizentalFragmentation(const Table &db, const Queries &queries, const PredicateGroups &predicates)
 		: db(db), queries(queries), predicates(predicates) { }
 
 	void run()
@@ -199,26 +281,34 @@ vector<string> split(const std::string &s, const char delim)
 /**
  * only the non-empty line strings should be passed through here
  */
-Table parseTable(vector<string> lines)
+Table parseTable(const vector<string> &lines)
 {
 	Table table;
 	vector<ColumnName> columnNames;
-	for (int i = 0; i < lines.size(); i ++)
+	for (unsigned i = 0; i < lines.size(); i ++)
 	{
 		auto tokens = split(lines[i], '\t');
 		if (i == 0)
 		{
 			// the fisrt record: attributes
 			columnNames = tokens;
+
+			// convert to upper cases
+			for (auto &str : columnNames)
+			{
+				transform(str.begin(), str.end(), str.begin(), ::toupper);
+			}
+
+			// create new table with thoe columns
 			table = Table(columnNames);
 		}
 		else
 		{
 			// the rest records
 			Record record;
-			for (int j = 0; j < columnNames.size(); j ++)
+			for (unsigned j = 0; j < columnNames.size(); j ++)
 			{
-				record.insert(columnNames[j], tokens[j]);
+				record[columnNames[j]] = tokens[j];
 			}
 			table.add(record);
 		}
@@ -229,17 +319,89 @@ Table parseTable(vector<string> lines)
 /**
  * only the non-empty line strings should be passed through here
  */
-PredicateGroups parsePredicateGroups(vector<string> lines)
+PredicateGroups parsePredicateGroups(const vector<string> &lines)
 {
-	// TODO:
+	PredicateGroups pgs;
+	for (const auto &line : lines)
+	{
+		PredicateGroup predicates;
+		string attr; // the attribute name used for the map
+
+		auto tokens = split(line, '\t');
+		string keyTemp;
+		for (const auto &token : tokens)
+		{
+			for (const auto &opMapper : OP_STRING_MAPPER)
+			{
+				const auto idx = token.find(opMapper.first);
+				if (idx != string::npos)
+				{
+					const Predicate p = Predicate(token.substr(0, idx), opMapper.second, token.substr(idx + opMapper.first.length()));
+					predicates.push_back(p);
+
+					// set key for the attribute map
+					if (keyTemp.empty()) keyTemp = p.key;
+					break;
+				}
+			}
+		}
+		pgs.insert({ {keyTemp, predicates} });
+	}
+	return pgs;
 }
 
 /**
  * only the non-empty line strings should be passed through here
  */
-Queries parseQueries(vector<string> queries)
+Queries parseQueries(const vector<string> &lines, const vector<ColumnName> &columnNames)
 {
-	// TODO:
+	Queries queries;
+	for (const auto &line : lines)
+	{
+		Query q;
+
+		auto tokens = split(line, '\t');
+		for (unsigned i = 0; i < tokens.size(); i ++)
+		{
+			const auto &token = tokens[i];
+			const auto idxDash = token.find('-');
+			if (idxDash == string::npos || idxDash == 0)
+			{
+				// instant value (instant negative is also considered)
+				// TODO: the first column is primary key, may add PK support later
+				q.push_back(Predicate(columnNames[i + 1], EQUAL, token));
+			}
+			else
+			{
+				// <KEY>-<OP><VALUE>
+				const string key = token.substr(0, idxDash); // key can be extracted here
+				const string theRest = token.substr(idxDash + 1); // skip the dash itself
+				if (theRest == "?")
+				{
+					q.push_back(Predicate(key, EQUAL, theRest));
+				}
+				else if (theRest != "ALL")
+				{
+					// "ALL" means this attribute is ignored
+					for (const auto &opMapper : OP_STRING_MAPPER)
+					{
+						// find op and val
+						const auto idx = theRest.find(opMapper.first);
+						if (idx != string::npos)
+						{
+							const string val = theRest.substr(idx + opMapper.first.length());
+							const Predicate p = Predicate(key, opMapper.second, val);
+							q.push_back(p);
+							break;
+						}
+					}
+				}
+			}
+		}
+		queries.push_back(q);
+	}
+
+	return queries;
 }
 
 /**
@@ -255,36 +417,36 @@ int main(int argc, char **argv)
 	vector<string> lines;
 
 	// read database file
-	ifstream fileDatabase(FILE_DATABASE, ios::in);
-	while (fileDatabase >> line)
+	ifstream fileDatabase(FILE_DATABASE);
+	while (getline(fileDatabase, line))
 	{
 		if (line.empty()) continue;
 		lines.push_back(line);
 	}
 	fileDatabase.close();
-	auto inTable = parseTable(lines);
+	const auto inTable = parseTable(lines);
 
 	// read applications/queries
 	lines.clear();
-	ifstream fileApp(FILE_QUERY, ios::in);
-	while (fileApp >> line)
+	ifstream fileApp(FILE_QUERY);
+	while (getline(fileApp, line))
 	{
 		if (line.empty()) continue;
 		lines.push_back(line);
 	}
 	fileApp.close();
-	auto inQueries = parseQueries(lines);
+	const auto inQueries = parseQueries(lines, inTable.getPrototype());
 
 	// read predicates
 	lines.clear();
-	ifstream filePredicates(FILE_SIMPLE_PREDICATE, ios::in);
-	while (filePredicates >> line)
+	ifstream filePredicates(FILE_SIMPLE_PREDICATE);
+	while (getline(filePredicates, line))
 	{
 		if (line.empty()) continue;
 		lines.push_back(line);
 	}
 	filePredicates.close();
-	auto inPredicateGroups = parsePredicateGroups(lines);
+	const auto inPredicateGroups = parsePredicateGroups(lines);
 
 	// run the algorithm
 	PrimaryHorizentalFragmentation phf(inTable, inQueries, inPredicateGroups);
