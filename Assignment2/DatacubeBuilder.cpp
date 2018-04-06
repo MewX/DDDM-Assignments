6049c069fceae9d4bb6b6722535a1a17a3fd3037
MewX <imewx@qq.com>
2018-04-06 15:28:32 +0930

    init assignment 2

---
+++ Assignment2/DatacubeBuilder.cpp
@@ -0,0 +1,11 @@
+/**
+ * This file is the DDDM assignment 2.
+ *
+ * @author Yuanzhong Xia (a1700831)
+ */
+#include "DatacubeBuilder.h"
+
+void buildCuboid(std::string dbFileName)
+{
+	// TODO:
+}
---
+++ Assignment2/DatacubeBuilder.h
@@ -0,0 +1,13 @@
+/**
+ * This file is the DDDM assignment 2.
+ *
+ * @author Yuanzhong Xia (a1700831)
+ */
+#ifndef _DATACUBE_BUILDER_H_
+#define _DATACUBE_BUILDER_H_
+#include <string>
+
+// the required function
+void buildCuboid(std::string dbFileName);
+
+#endif
