3bbacc61cec9d16c0e37f1e3e7c0b478a20150ba
MewX <imewx@qq.com>
2018-05-04 16:05:32 +0930

    Added cuboid calculator

--- Assignment2/DatacubeBuilder.cpp
+++ Assignment2/DatacubeBuilder.cpp
@@ -5,21 +5,159 @@
  */
 #include "DatacubeBuilder.h"
 #include <string>
+#include <vector>
+#include <sstream>
+#include <cassert>
+#include <fstream>
 
 using namespace std;
 
 /**
  * the constructor
  */
-DatacubeBuilder::DatacubeBuilder()
+DatacubeBuilder::DatacubeBuilder() :
+	UNIVERSITY_CODE_TABLE({ {"UofA", 0}, {"UniSA", 1}, {"Flinders", 2}, {"Torrens", 3} }),
+	PROGRAM_CODE_TABLE({ {"PhD", 0}, {"MCS", 1}, {"MSE", 2}, {"MCI", 3}, {"BCS", 4}, {"BSE", 5} }),
+	TERM_CODE_TABLE({ {"s1/2016", 0}, {"s2/2016", 1}, {"s1/2017", 2}, {"s2/2017", 3}, {"s1/2018", 4} }),
+	NATIONALITY_CODE_TABLE({ {"Australia", 0}, {"Korea", 1}, {"China", 2}, {"India", 3}, {"Iran", 4}, {"SriLanka", 5} }),
+	SCHOLARSHIP_CODE_TABLE({ {"No", 0}, {"Yes", 1} })
 {
-	// TODO:
+}
+
+/**
+ * look for the value from the input table
+ */
+int DatacubeBuilder::findValueFromKey(const CODE_TABLE table, const std::string key) const
+{
+	for (const auto ele : table)
+	{
+		if (ele.first == key)
+		{
+			return ele.second;
+		}
+	}
+	assert(false); // should not run into here from the given input
+}
+
+/**
+ * The split function for C++ string.
+ */
+vector<string> DatacubeBuilder::split(const string &s, const char delim)
+{
+	vector<string> elems;
+	stringstream ss(s);
+	string item;
+	while (getline(ss, item, delim))
+	{
+		if (!item.empty())
+		{
+			elems.push_back(item);
+		}
+	}
+	return elems;
 }
 
 /**
  * the required function
  */
-void DatacubeBuilder::buildCuboid(char *dbFileName)
+void DatacubeBuilder::buildCuboid(const char *dbFileName)
 {
-	// TODO:
+	// read all lines from the database file (column order)
+	// University	Program	Term	Nationality	Scholarship	Enrolment
+	const string DB_FILE_NAME = dbFileName;
+	string line;
+	vector<string> lines;
+	ifstream fileDatabase(DB_FILE_NAME);
+	while (getline(fileDatabase, line))
+	{
+		if (line.empty()) continue;
+		lines.push_back(line);
+	}
+	fileDatabase.close();
+
+	// build from 5-D cuboid (skipping the first column name)
+	build5dCuboid(lines);
+
+	// build all other cuboids based on 5-D cuboid
+	buildAllLowerCuboids();
+}
+
+void DatacubeBuilder::build5dCuboid(const vector<string> lines)
+{
+	for (unsigned i = 1; i < lines.size(); i ++)
+	{
+		// University, Program, Term, Nationality, Scholarship, Enrolment
+		auto tokens = split(lines[i], '\t');
+		const int uni = findValueFromKey(UNIVERSITY_CODE_TABLE, tokens[0]);
+		const int program = findValueFromKey(PROGRAM_CODE_TABLE, tokens[1]);
+		const int term = findValueFromKey(TERM_CODE_TABLE, tokens[2]);
+		const int nation = findValueFromKey(NATIONALITY_CODE_TABLE, tokens[3]);
+		const int scholarship = findValueFromKey(SCHOLARSHIP_CODE_TABLE, tokens[4]);
+		const int enrolment = atoi(tokens[5].c_str());
+
+		// set it
+		uptnsCuboid[uni][program][term][nation][scholarship] = enrolment;
+	}
+}
+
+void DatacubeBuilder::buildAllLowerCuboids()
+{
+	// update uptn
+	for (unsigned uni = 0; uni < UNIVERSITY_CODE_TABLE.size(); uni ++)
+	{
+		for (unsigned program = 0; program < PROGRAM_CODE_TABLE.size(); program ++)
+		{
+			for (unsigned term = 0; term < TERM_CODE_TABLE.size(); term ++)
+			{
+				for (unsigned nation = 0; nation < NATIONALITY_CODE_TABLE.size(); nation ++)
+				{
+					for (unsigned scholar = 0; scholar < SCHOLARSHIP_CODE_TABLE.size(); scholar ++)
+					{
+						const int temp = uptnsCuboid[uni][program][term][nation][scholar];
+
+						// 4d cuboids
+						uptnCuboid[uni][program][term][nation] += temp;
+						uptsCuboid[uni][program][term][scholar] += temp;
+						upnsCuboid[uni][program][nation][scholar] += temp;
+						utnsCuboid[uni][term][nation][scholar] += temp;
+						ptnsCuboid[program][term][nation][scholar] += temp;
+
+						// 3d cuboids
+						uptCuboid[uni][program][term] += temp;
+						upnCuboid[uni][program][nation] += temp;
+						upsCuboid[uni][program][scholar] += temp;
+						utnCuboid[uni][term][nation] += temp;
+						utsCuboid[uni][term][scholar] += temp;
+						unsCuboid[uni][nation][scholar] += temp;
+						ptnCuboid[program][term][nation] += temp;
+						ptsCuboid[program][term][scholar] += temp;
+						pnsCuboid[program][nation][scholar] += temp;
+						tnsCuboid[term][nation][scholar] += temp;
+
+						// 2d cuboids
+						upCuboid[uni][program] += temp;
+						utCuboid[uni][term] += temp;
+						unCuboid[uni][nation] += temp;
+						usCuboid[uni][scholar] += temp;
+						ptCuboid[program][term] += temp;
+						pnCuboid[program][nation] += temp;
+						psCuboid[program][scholar] += temp;
+						tnCuboid[term][nation] += temp;
+						tsCuboid[term][scholar] += temp;
+						nsCuboid[nation][scholar] += temp;
+
+						// 1d cuboids
+						uCuboid[uni] += temp;
+						pCuboid[program] += temp;
+						tCuboid[term] += temp;
+						nCuboid[nation] += temp;
+						sCuboid[scholar] += temp;
+
+						// all
+						all += temp;
+					}
+				}
+			}
+		}
+	}
 }
--- Assignment2/DatacubeBuilder.h
+++ Assignment2/DatacubeBuilder.h
@@ -5,48 +5,66 @@
  */
 #ifndef _DATACUBE_BUILDER_H_
 #define _DATACUBE_BUILDER_H_
+#include <string>
+#include <vector>
+#include <map>
+
+// types
+typedef std::vector<std::pair<std::string, int>> CODE_TABLE;
 
 class DatacubeBuilder {
+private:
+	const CODE_TABLE UNIVERSITY_CODE_TABLE;
+	const CODE_TABLE PROGRAM_CODE_TABLE;
+	const CODE_TABLE TERM_CODE_TABLE;
+	const CODE_TABLE NATIONALITY_CODE_TABLE;
+	const CODE_TABLE SCHOLARSHIP_CODE_TABLE;
+
+	int findValueFromKey(const CODE_TABLE table, const std::string key) const;
+
+	void build5dCuboid(const std::vector<std::string> lines);
+	void buildAllLowerCuboids();
+
 public:
 	//5-D Cuboid
 	//University-Program-Term-Nationality-Scholarship
-	int uptnsCuboid[4][6][5][6][2];
+	int uptnsCuboid[4][6][5][6][2] = {0};
 	//4-D Cuboid
-	int uptnCuboid[4][6][5][6];
-	int uptsCuboid[4][6][5][2];
-	int upnsCuboid[4][6][6][2];
-	int utnsCuboid[4][5][6][2];
-	int ptnsCuboid[6][5][6][2];
+	int uptnCuboid[4][6][5][6] = {0};
+	int uptsCuboid[4][6][5][2] = {0};
+	int upnsCuboid[4][6][6][2] = {0};
+	int utnsCuboid[4][5][6][2] = {0};
+	int ptnsCuboid[6][5][6][2] = {0};
 	//3-D Cuboid
-	int uptCuboid[4][6][5];
-	int upnCuboid[4][6][6];
-	int upsCuboid[4][6][2];
-	int utnCuboid[4][5][6];
-	int utsCuboid[4][5][2];
-	int unsCuboid[3][6][2];
-	int ptnCuboid[6][5][6];
-	int ptsCuboid[6][5][2];
-	int pnsCuboid[6][6][2];
-	int tnsCuboid[5][6][2];
+	int uptCuboid[4][6][5] = {0};
+	int upnCuboid[4][6][6] = {0};
+	int upsCuboid[4][6][2] = {0};
+	int utnCuboid[4][5][6] = {0};
+	int utsCuboid[4][5][2] = {0};
+	int unsCuboid[3][6][2] = {0};
+	int ptnCuboid[6][5][6] = {0};
+	int ptsCuboid[6][5][2] = {0};
+	int pnsCuboid[6][6][2] = {0};
+	int tnsCuboid[5][6][2] = {0};
 	//2-D Cuboid
-	int upCuboid[4][6];
-	int utCuboid[4][5];
-	int unCuboid[4][6];
-	int usCuboid[4][2];
-	int ptCuboid[6][5];
-	int pnCuboid[6][6];
-	int psCuboid[6][2];
-	int tnCuboid[5][6];
-	int tsCuboid[5][2];
-	int nsCuboid[6][2];
+	int upCuboid[4][6] = {0};
+	int utCuboid[4][5] = {0};
+	int unCuboid[4][6] = {0};
+	int usCuboid[4][2] = {0};
+	int ptCuboid[6][5] = {0};
+	int pnCuboid[6][6] = {0};
+	int psCuboid[6][2] = {0};
+	int tnCuboid[5][6] = {0};
+	int tsCuboid[5][2] = {0};
+	int nsCuboid[6][2] = {0};
 	//1-D Cuboid
-	int uCuboid[4];
-	int pCuboid[6];
-	int tCuboid[5];
-	int nCuboid[6];
-	int sCuboid[2];
+	int uCuboid[4] = {0};
+	int pCuboid[6] = {0};
+	int tCuboid[5] = {0};
+	int nCuboid[6] = {0};
+	int sCuboid[2] = {0};
 	//0-D Cuboid
-	int all;
+	int all = 0;
 
 	// constructor
 	DatacubeBuilder();
@@ -55,7 +73,9 @@ public:
 	// Please make it as a public function.
 	// The parameter to this function will be argv[1] to the main() in our driver
 	// i.e. the database.txt
-	void buildCuboid(char *);
+	void buildCuboid(const char *);
+
+	std::vector<std::string> split(const std::string &s, const char delim);
 };
 
 #endif
